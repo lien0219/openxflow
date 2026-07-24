007c684d88eb651a29ef15bbd692d769a4815bfe
LIEN <lien0001@163.com>
2026-07-24 18:20:28 +0800

    feat(frontend): query paginated channel execution logs

---
+++ src/frontend/src/controllers/API/queries/channels/use-get-channel-executions.ts
@@ -0,0 +1,53 @@
+import type { UseQueryResult } from "@tanstack/react-query";
+import type { useQueryFunctionType } from "@/types/api";
+import { api } from "../../api";
+import { getURL } from "../../helpers/constants";
+import { UseRequestProcessor } from "../../services/request-processor";
+import type {
+  ChannelExecutionLogPage,
+  ChannelExecutionQuery,
+} from "./types";
+
+export const useGetChannelExecutions: useQueryFunctionType<
+  ChannelExecutionQuery,
+  ChannelExecutionLogPage
+> = (params, options) => {
+  const { query } = UseRequestProcessor();
+
+  const getExecutions = async (): Promise<ChannelExecutionLogPage> => {
+    const response = await api.get<ChannelExecutionLogPage>(
+      `${getURL("CHANNELS")}/${params.connectionId}/executions`,
+      {
+        params: {
+          page: params.page ?? 1,
+          page_size: params.pageSize ?? 20,
+          conversation_binding_id:
+            params.conversationBindingId || undefined,
+          openxflow_user_id: params.openxflowUserId || undefined,
+          status: params.status || undefined,
+          trigger_type: params.triggerType || undefined,
+        },
+      },
+    );
+    return response.data;
+  };
+
+  return query(
+    [
+      "useGetChannelExecutions",
+      params.connectionId,
+      params.page ?? 1,
+      params.pageSize ?? 20,
+      params.conversationBindingId ?? "",
+      params.openxflowUserId ?? "",
+      params.status ?? "",
+      params.triggerType ?? "",
+    ],
+    getExecutions,
+    {
+      enabled: Boolean(params.connectionId),
+      refetchOnWindowFocus: false,
+      ...options,
+    },
+  ) as UseQueryResult<ChannelExecutionLogPage, Error>;
+};
