604b88d3d9d8757fc11a727fe2729197d77953f9
LIEN <lien0001@163.com>
2026-07-24 18:20:12 +0800

    feat(frontend): query paginated channel commands

---
+++ src/frontend/src/controllers/API/queries/channels/use-get-channel-commands.ts
@@ -0,0 +1,50 @@
+import type { UseQueryResult } from "@tanstack/react-query";
+import type { useQueryFunctionType } from "@/types/api";
+import { api } from "../../api";
+import { getURL } from "../../helpers/constants";
+import { UseRequestProcessor } from "../../services/request-processor";
+import type {
+  ChannelCommandQuery,
+  ChannelWorkflowCommandPage,
+} from "./types";
+
+export const useGetChannelCommands: useQueryFunctionType<
+  ChannelCommandQuery,
+  ChannelWorkflowCommandPage
+> = (params, options) => {
+  const { query } = UseRequestProcessor();
+
+  const getCommands = async (): Promise<ChannelWorkflowCommandPage> => {
+    const response = await api.get<ChannelWorkflowCommandPage>(
+      `${getURL("CHANNELS")}/${params.connectionId}/commands`,
+      {
+        params: {
+          page: params.page ?? 1,
+          page_size: params.pageSize ?? 20,
+          query: params.query || undefined,
+          scope_type: params.scopeType || undefined,
+          enabled: params.enabled,
+        },
+      },
+    );
+    return response.data;
+  };
+
+  return query(
+    [
+      "useGetChannelCommands",
+      params.connectionId,
+      params.page ?? 1,
+      params.pageSize ?? 20,
+      params.query ?? "",
+      params.scopeType ?? "",
+      params.enabled ?? "all",
+    ],
+    getCommands,
+    {
+      enabled: Boolean(params.connectionId),
+      refetchOnWindowFocus: false,
+      ...options,
+    },
+  ) as UseQueryResult<ChannelWorkflowCommandPage, Error>;
+};
